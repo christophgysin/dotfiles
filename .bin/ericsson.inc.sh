442105f0d933c17017bc111f5700bf79c19357ae
Christoph Gysin <christoph.gysin@gmail.com>
2013-09-24 21:15:09 +0300

    add mail forward, make xmpp host configureable

--- .bin/ericsson.inc.sh
+++ .bin/ericsson.inc.sh
@@ -5,4 +5,7 @@ access_host=svi.ericsson.se
 internal_host=esekilxxen985.rnd.ericsson.se
 ssh_port=10022
 socks_port=10023
+xmpp_host=sami-laptop.lmf.ericsson.se
 xmpp_port=5222
+mail_host=mail.internal.ericsson.se
+mail_port=10443
