a1b12464d7dd5f31904e71eac5adc54d40385add
Christoph Gysin <christoph.gysin@gmail.com>
2014-07-03 11:40:42 +0300

    use 981

--- .bin/ericsson.inc.sh
+++ .bin/ericsson.inc.sh
@@ -2,7 +2,7 @@
 
 user=echrgys
 access_host=svi.ericsson.se
-internal_host=esekilxxen985.rnd.ericsson.se
+internal_host=esekilxxen981.rnd.ericsson.se
 ssh_port=10022
 socks_port=10023
 xmpp_host=sami-laptop.lmf.ericsson.se
