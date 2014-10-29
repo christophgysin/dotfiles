ad70ff80e5f5b1abaed3478f54112eb9dea2b668
Christoph Gysin <christoph.gysin@gmail.com>
2014-10-29 21:58:05 +0200

    ericsson: updated

--- .bin/ericsson.inc.sh
+++ .bin/ericsson.inc.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 user=echrgys
-access_host=svi.ericsson.se
-internal_host=esekilxxen981.rnd.ericsson.se
+
 ssh_port=10022
 socks_port=10023
-xmpp_host=sami-laptop.lmf.ericsson.se
-xmpp_port=5222
-mail_host=mail.internal.ericsson.se
-mail_port=10443
