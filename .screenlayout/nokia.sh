a6003a38536e5150d36533a01d4d47ba3af3fdbe
Christoph Gysin <christoph.gysin@gmail.com>
2017-10-21 22:55:46 +0300

    remove N

--- .screenlayout/nokia.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-xrandr \
-  --output DP-2 --auto --pos 0x0 --primary \
-  --output eDP-1 --auto --scale 0.5x0.5 --pos 1680x536 \
-  --output DP-1 --off \
-  --output HDMI-1 --off \
-  --output HDMI-2 --off \
-
