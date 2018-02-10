f8548c7fc150a808b4d4bc662e3bac95796cb818
Christoph Gysin <christoph.gysin@gmail.com>
2018-02-10 11:58:52 +0200

    outputs: fix left

--- .screenlayout/left.sh
+++ .screenlayout/left.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 xrandr \
-  --output eDP-1 --scale 0.5x0.5 --mode 2560x1600 --pos 0x1208 --rotate normal \
+  --output eDP-1 --scale 0.5x0.5 --mode 2560x1600 --pos 3840x1208 --rotate normal \
   --output DP-1 --off \
-  --output DP-2 --primary --mode 3840x1600 --pos 1280x0 --rotate normal \
+  --output DP-2 --primary --mode 3840x1600 --rotate normal \
   --output HDMI-1 --off \
-  --output HDMI-2 --off \
-
+  --output HDMI-2 --off
